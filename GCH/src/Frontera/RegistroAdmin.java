730164ca797f2546fd90d9e50b40f3a338b26100
DAVID <DAVID@DESKTOP-16SR7BD>
2020-05-22 12:01:33 -0500

    Se unen clases ValidarRegistroAdministrador con RegistroAdministrador

--- GCH/src/Frontera/RegistroAdmin.java
+++ GCH/src/Frontera/RegistroAdmin.java
@@ -5,6 +5,11 @@
  */
 package Frontera;
 
+import Control.ValidarRegistroAdministrador;
+import Entidad.Administrador;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author tech
@@ -29,9 +34,9 @@ public class RegistroAdmin extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel4 = new javax.swing.JLabel();
-        adminNombreTF = new javax.swing.JTextField();
+        adminNombre1TF = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
-        adminApellidoTF = new javax.swing.JTextField();
+        adminApellido1TF = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
         adminIdTF = new javax.swing.JTextField();
         jLabel7 = new javax.swing.JLabel();
@@ -44,30 +49,30 @@ public class RegistroAdmin extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        adminNombre2TF = new javax.swing.JTextField();
+        jLabel9 = new javax.swing.JLabel();
+        adminApellido2TF = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
         jLabel4.setBackground(new java.awt.Color(0, 0, 0));
-        jLabel4.setForeground(new java.awt.Color(0, 0, 0));
         jLabel4.setText("Celular");
 
-        adminNombreTF.setBackground(new java.awt.Color(102, 102, 102));
+        adminNombre1TF.setBackground(new java.awt.Color(102, 102, 102));
 
         jLabel5.setBackground(new java.awt.Color(0, 0, 0));
-        jLabel5.setForeground(new java.awt.Color(0, 0, 0));
         jLabel5.setText("Email");
 
-        adminApellidoTF.setBackground(new java.awt.Color(102, 102, 102));
+        adminApellido1TF.setBackground(new java.awt.Color(102, 102, 102));
 
         jLabel6.setBackground(new java.awt.Color(0, 0, 0));
-        jLabel6.setForeground(new java.awt.Color(0, 0, 0));
         jLabel6.setText("Usuario");
 
         adminIdTF.setBackground(new java.awt.Color(102, 102, 102));
 
-        jLabel7.setForeground(new java.awt.Color(0, 0, 0));
         jLabel7.setText("Contraseña");
 
         adminCelularTF.setBackground(new java.awt.Color(102, 102, 102));
@@ -88,60 +93,80 @@ public class RegistroAdmin extends javax.swing.JFrame {
         adminContrasenniaTF.setBackground(new java.awt.Color(102, 102, 102));
 
         jLabel1.setBackground(new java.awt.Color(0, 0, 0));
-        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
-        jLabel1.setText("Nombres");
+        jLabel1.setText("Primer Nombre");
 
         jLabel2.setBackground(new java.awt.Color(0, 0, 0));
-        jLabel2.setForeground(new java.awt.Color(0, 0, 0));
-        jLabel2.setText("Apellidos");
+        jLabel2.setText("Primer Apellido");
 
         jLabel3.setBackground(new java.awt.Color(0, 0, 0));
-        jLabel3.setForeground(new java.awt.Color(0, 0, 0));
         jLabel3.setText("No. Identificación");
+
+        jLabel8.setText("Segundo Nombre");
+
+        jLabel9.setBackground(new java.awt.Color(0, 0, 0));
+        jLabel9.setText("Segundo Apellido");
+
+        adminApellido2TF.setBackground(new java.awt.Color(102, 102, 102));
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(111, 111, 111)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel4)
-                    .addComponent(jLabel5)
-                    .addComponent(jLabel6)
-                    .addComponent(jLabel7))
-                .addGap(28, 28, 28)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(adminApellidoTF, javax.swing.GroupLayout.DEFAULT_SIZE, 120, Short.MAX_VALUE)
-                    .addComponent(adminIdTF, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(adminCelularTF, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(adminEmailTF, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(adminUsuarioTF, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(adminContrasenniaTF, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(adminNombreTF))
-                .addContainerGap(282, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(cancelarB, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(aceptarB, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(42, 42, 42))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(111, 111, 111)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel9)
+                        .addGap(30, 30, 30)
+                        .addComponent(adminApellido2TF, javax.swing.GroupLayout.DEFAULT_SIZE, 120, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel6)
+                            .addComponent(jLabel7)
+                            .addComponent(jLabel8))
+                        .addGap(28, 28, 28)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(adminApellido1TF, javax.swing.GroupLayout.DEFAULT_SIZE, 120, Short.MAX_VALUE)
+                            .addComponent(adminIdTF, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(adminCelularTF, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(adminEmailTF, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(adminUsuarioTF, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(adminContrasenniaTF, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(adminNombre1TF)
+                            .addComponent(adminNombre2TF))))
+                .addContainerGap(282, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(44, 44, 44)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(adminNombreTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(adminNombre1TF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1))
-                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(adminApellidoTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel8)
+                    .addComponent(adminNombre2TF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(adminApellido1TF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2))
-                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel9)
+                    .addComponent(adminApellido2TF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(5, 5, 5)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(adminIdTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3))
@@ -161,7 +186,7 @@ public class RegistroAdmin extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(adminContrasenniaTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel7))
-                .addGap(110, 110, 110)
+                .addGap(84, 84, 84)
                 .addComponent(aceptarB)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(cancelarB)
@@ -175,9 +200,9 @@ public class RegistroAdmin extends javax.swing.JFrame {
             .addGap(0, 649, Short.MAX_VALUE)
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(layout.createSequentialGroup()
-                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addGap(0, 12, Short.MAX_VALUE)
                     .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGap(0, 0, Short.MAX_VALUE)))
+                    .addGap(0, 12, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -193,7 +218,21 @@ public class RegistroAdmin extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void aceptarBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_aceptarBActionPerformed
-        // TODO add your handling code here:
+        Administrador admin = new Administrador();
+        admin.setNombre1(adminNombre1TF.getText());
+        admin.setNombre2(adminNombre2TF.getText());
+        admin.setApellido1(adminApellido1TF.getText());
+        admin.setApellido2(adminApellido2TF.getText());
+        admin.setNidentificacion(adminIdTF.getText());
+        admin.setCelular(adminCelularTF.getText());
+        admin.setEmail(adminEmailTF.getText());
+        admin.setUsuario(adminUsuarioTF.getText());
+        admin.setContrasenia(adminContrasenniaTF.getText());
+        
+        ValidarRegistroAdministrador validar = new ValidarRegistroAdministrador();
+        System.out.println("-------");
+        String resultado = validar.VerificarRegistroAdministrador(admin);
+        System.out.println(resultado);
     }//GEN-LAST:event_aceptarBActionPerformed
 
     /**
@@ -234,12 +273,14 @@ public class RegistroAdmin extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton aceptarB;
-    private javax.swing.JTextField adminApellidoTF;
+    private javax.swing.JTextField adminApellido1TF;
+    private javax.swing.JTextField adminApellido2TF;
     private javax.swing.JTextField adminCelularTF;
     private javax.swing.JTextField adminContrasenniaTF;
     private javax.swing.JTextField adminEmailTF;
     private javax.swing.JTextField adminIdTF;
-    private javax.swing.JTextField adminNombreTF;
+    private javax.swing.JTextField adminNombre1TF;
+    private javax.swing.JTextField adminNombre2TF;
     private javax.swing.JTextField adminUsuarioTF;
     private javax.swing.JButton cancelarB;
     private javax.swing.JLabel jLabel1;
@@ -249,6 +290,8 @@ public class RegistroAdmin extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
