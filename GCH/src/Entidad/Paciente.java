bf63dcaa83b4936198d1253385dcdc8408595c36
DAVID <DAVID@DESKTOP-16SR7BD>
2020-05-24 21:16:21 -0500

    Funciona: Autenticacion(Personal y Admin) Registro(Personal, Admin y Equipo) IngresoPaciente

--- GCH/src/Entidad/Paciente.java
+++ GCH/src/Entidad/Paciente.java
@@ -23,10 +23,10 @@ public class Paciente implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idPaciente;
-    private String nombrePaciente1;
-    private String nombrePaciente2;
-    private String apellidoPaciente1;
-    private String apellidoPaciente2;
+    private String nombre1;
+    private String nombre2;
+    private String apellido1;
+    private String apellido2;
     private String TipoSangre;
 
     public Paciente() {
@@ -41,35 +41,35 @@ public class Paciente implements Serializable {
     }
 
     public String getNombrePaciente1() {
-        return nombrePaciente1;
+        return nombre1;
     }
 
     public void setNombrePaciente1(String nombrePaciente1) {
-        this.nombrePaciente1 = nombrePaciente1;
+        this.nombre1 = nombrePaciente1;
     }
 
     public String getNombrePaceinte2() {
-        return nombrePaciente2;
+        return nombre2;
     }
 
     public void setNombrePaceinte2(String nombrePaceinte2) {
-        this.nombrePaciente2 = nombrePaceinte2;
+        this.nombre2 = nombrePaceinte2;
     }
 
     public String getApellidoPaciente1() {
-        return apellidoPaciente1;
+        return apellido1;
     }
 
     public void setApellidoPaciente1(String apellidoPaciente1) {
-        this.apellidoPaciente1 = apellidoPaciente1;
+        this.apellido1 = apellidoPaciente1;
     }
 
     public String getApellidoPaciente2() {
-        return apellidoPaciente2;
+        return apellido2;
     }
 
     public void setApellidoPaciente2(String apellidoPaciente2) {
-        this.apellidoPaciente2 = apellidoPaciente2;
+        this.apellido2 = apellidoPaciente2;
     }
 
     public String getTipoSangre() {
