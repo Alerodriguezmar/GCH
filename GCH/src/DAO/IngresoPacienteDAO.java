57870800fff737fd96008d945354f051aa59e2b2
DAVID <DAVID@DESKTOP-16SR7BD>
2020-06-04 14:25:41 -0500

    LeerPacientePorIda clase IngresoPaciente

--- GCH/src/DAO/IngresoPacienteDAO.java
+++ GCH/src/DAO/IngresoPacienteDAO.java
@@ -6,6 +6,12 @@
 package DAO;
 
 import Entidad.IngresoPaciente;
+import Entidad.Paciente;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.NonUniqueResultException;
@@ -103,5 +109,62 @@ public class IngresoPacienteDAO {
             em.close();
             return ret;
         }
+    }
+    
+    public Paciente leerPacientePorId(String id){
+        
+        System.out.println("BUSCANDO PACIENTE...");
+        Paciente p = new Paciente();
+        String query = "SELECT * FROM INGRESO_PACIENTE WHERE PACIENTE_IDPACIENTE="+id+" AND ESTADO = 1";
+        String url = "jdbc:derby://localhost:1527/GCHDB_JPA";
+        String username = "root";
+        String password = "123456";
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            conn = DriverManager.getConnection(url, username, password);
+            stmt = conn.createStatement();
+            ResultSet res = stmt.executeQuery(query);
+                
+            if(res.next() == true){
+                query = "SELECT * FROM PACIENTES WHERE IDPACIENTE="+id;
+                try {
+                    res = stmt.executeQuery(query);
+                    if(res.next()){
+                        System.out.println("Paciente Encontrado...");
+                        System.out.println(res.getInt("idPaciente") +      
+                        res.getString("nombre1") + ", " +
+                        res.getString("nombre2") + ", " +
+                        res.getString("apellido1") + ", "+
+                        res.getString("apellido2") + ", "+
+                        res.getString("tiposangre"));
+                //creo paciente
+                        p.setNombrePaciente2(res.getString("nombre2"));
+                        p.setNombrePaciente1(res.getString("nombre1"));
+                        p.setApellidoPaciente1(res.getString("apellido1"));
+                        p.setApellidoPaciente2(res.getString("apellido2"));   
+                        p.setTipoSangre(res.getString("tiposangre"));
+
+                        System.out.println("Id: "+p.getIdPaciente()+" Nombre: "+p.getNombrePaciente1());
+                    }
+                    else{
+                        System.out.println("Paciente NO ENCONTRADO");
+                    }
+                }
+                catch(SQLException e){
+                    e.printStackTrace();
+                }
+                
+            }else{ 
+                System.out.println("INGRESO no encontrado");          
+            }
+            res.close();
+            stmt.execute(query);
+            conn.close();
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } 
+        return p;
     }
 }
