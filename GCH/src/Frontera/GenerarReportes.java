e7f1a87a1a957c019ef96edce02d8b2150952095
DAVID <DAVID@DESKTOP-16SR7BD>
2020-06-08 19:10:24 -0500

    Reportes jTables

--- GCH/src/Frontera/GenerarReportes.java
+++ GCH/src/Frontera/GenerarReportes.java
@@ -286,9 +286,16 @@ public class GenerarReportes extends javax.swing.JPanel {
     public String [][] mostrarPersonal(){
         PersonalMedicoDAO pmdao = new PersonalMedicoDAO();
         List<PersonalMedico> pms = new ArrayList();
-        PersonalMedico p = new PersonalMedico();
-        p.setCargo("MÉDICO GENERAL");
-        pms.add(p);
+        
+        
+        String[] nombres = new String[] {"Asistente enfermería", "Asistente médico profesional", "Médico especialista", "Médico general", "Profesional en enfermería" };
+        
+        for(int i = 0; i < nombres.length; i++){
+            PersonalMedico p = new PersonalMedico();
+            p.setCargo(nombres[i]);
+            pms.add(p);         
+        }
+        
         String[][] matriz=new String[pms.size()][2];
         for(int i = 0; i<pms.size(); i++){
             matriz[i][0] = pms.get(i).getCargo();
@@ -296,31 +303,22 @@ public class GenerarReportes extends javax.swing.JPanel {
         }
         return matriz;        
     }
+    
     public String [][] mostrarEquipo(){
         EquipoDAO edao = new EquipoDAO();
         List<Equipo> eqs = new ArrayList();
-        Equipo e = new Equipo();
-        e.setNombreEquipo("VENTILADOR MECANICO");
-        eqs.add(e);
-        e.setNombreEquipo("SATUROMETRO");
-        eqs.add(e);
-        e.setNombreEquipo("BALA OXIGENO");
-        eqs.add(e);
-        e.setNombreEquipo("MONITOR");
-        eqs.add(e);
-        e.setNombreEquipo("BOMBA INFUSION CONTINUA");
-        eqs.add(e);
-        e.setNombreEquipo("BOMBA NUTRICION ENTERAL");
-        eqs.add(e);
-        e.setNombreEquipo("ASPIRADOR DE SECRECIONES");
-        eqs.add(e);
-        e.setNombreEquipo("EQUIPO DE INTUBACION");
-        eqs.add(e);
         
+        String[] nombres = new String[] {"EQUIPO DE INTUBACION", "SATUROMETRO", "BALA OXIGENO", "MONITOR", "BOMBA INFUSION CONTINUA", "BOMBA NUTRICION ENTERAL", "ASPIRADOR DE SECRECIONES","VENTILADOR MECANICO" };
+        for(int i = 0; i < nombres.length; i++){
+            Equipo e = new Equipo();
+            e.setIdEquipo(i);
+            e.setNombreEquipo(nombres[i]);
+            eqs.add(e); 
+        }
         String[][] matriz=new String[eqs.size()][2];
         for(int i = 0; i<eqs.size(); i++){
             matriz[i][0] = eqs.get(i).getNombreEquipo();
-            matriz[i][1] = Integer.toString(10);//10 se debe reemplazar por la consulta SQL
+            matriz[i][1] =Long.toString(edao.leerEq(eqs.get(i).getNombreEquipo()));//10 se debe reemplazar por la consulta SQL
         }
         return matriz;        
     }
--- GCH/src/Frontera/IngresoPacienteFrontera.java
+++ GCH/src/Frontera/IngresoPacienteFrontera.java
@@ -858,29 +858,8 @@ public class IngresoPacienteFrontera extends javax.swing.JPanel {
             matriz[i][0] = Integer.toString(eqs.get(i).getIdEquipo());
             matriz[i][1] = eqs.get(i).getNombreEquipo();
             matriz[i][2] = Long.toString(edao.leerEq(eqs.get(i).getNombreEquipo()));//10 se debe reemplazar por la consulta SQL
-            
-            
         }
         
         return matriz;
-        
-        /*EquipoDAO edao = new EquipoDAO();
-        List<Equipo> eqs = new ArrayList();
-        Equipo e = new Equipo();
-        String[] nombres;
-        nombres = new String[] {"EQUIPO DE INTUBACION", "SATUROMETRO", "BALA OXIGENO", "MONITOR", "BOMBA INFUSION CONTINUA", "BOMBA NUTRICION ENTERAL", "ASPIRADOR DE SECRECIONES","VENTILADOR MECANICO" };
-        for(int i = 0; i < nombres.length; i++){
-            e.setIdEquipo(i);
-            e.setNombreEquipo(nombres[i]);
-            eqs.add(e);
-        }
-        
-        String[][] matriz=new String[eqs.size()][3];
-        for(int i = 0; i<eqs.size(); i++){
-            matriz[i][0] = Integer.toString(eqs.get(i).getIdEquipo());
-            matriz[i][1] = eqs.get(i).getNombreEquipo();
-            matriz[i][2] = Long.toString(edao.leerEq(eqs.get(i).getNombreEquipo()));//10 se debe reemplazar por la consulta SQL
-        }
-        return matriz;       */ 
     }
 }
